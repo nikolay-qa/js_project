e78592cdad3b8f62b071b02eefd05d054814ecaf
Nikolay Romeiko <nickolay.romeyko@gmail.com>
2020-05-24 21:16:51 +0300

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,48 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+// Gets input from input field
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+// Generates and writes calculation log
+function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription); // from vendor file
+}
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteLog('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteLog('-', initialResult, enteredNumber);
+
+}
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteLog('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteLog('/', initialResult, enteredNumber);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
+
